2a47d9a3c9519fbe8b15830d2f98fa396c7cd3ad
Gabbie99 <khoinom2018@gmail.com>
2022-11-22 16:55:13 +0100

    working portfolio

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import AboutMe from './components/aboutMe/AboutMe';
 // import Home from './components/home/Home';
 import LandingPage from './components/landingPage/LandingPage';
+import Projects from './components/projects/Projects';
 // import NavBar from './components/navBar/NavBar';
 import Technical from './components/technical/Technical';
 
@@ -11,6 +12,7 @@ function App() {
       <LandingPage/>
       <AboutMe/>
       <Technical/>
+      <Projects/>
     </div>
   );
 }
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,13 +1,73 @@
 import React from 'react'
 import './Projects.css'
+import thumb1 from '../../assets/Capture1.JPG'
+import thumb2 from '../../assets/Capture2.JPG'
+import thumb3 from '../../assets/tech.jpg'
+// import thumb4 from '../../assets/Capture1.JPG'
 
 function Projects() {
   return (
     <div className='pro-gen'>
         <div className="project">
             <div className="pro-sec">
-                
+                <div>
+                    <img src={thumb1} alt="" />
+                    <h3></h3>
+                    <p></p>
+                    <div>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <div>
+                        <button>See Code</button>
+                        <button>View Live</button>
+                    </div>
+                </div>
+                <div>
+                    <img src={thumb2} alt="" />
+                    <h3></h3>
+                    <p></p>
+                    <div>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <div>
+                        <button>See Code</button>
+                        <button>View Live</button>
+                    </div>
+                </div>
+                <div>
+                    <img src={thumb3} alt="" />
+                    <h3></h3>
+                    <p></p>
+                    <div>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <div>
+                        <button>See Code</button>
+                        <button>View Live</button>
+                    </div>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h3></h3>
+                    <p></p>
+                    <div>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <div>
+                        <button>See Code</button>
+                        <button>View Live</button>
+                    </div>
+                </div>
             </div>
+            <p>View more &rarr;</p>
         </div>
     </div>
   )
