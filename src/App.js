5de4eccc9eebebbbd0ecfddd980f7a3aafbd1643
Gabbie99 <khoinom2018@gmail.com>
2022-11-22 12:44:31 +0100

    working portfolio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './components/home/Home';
 import NavBar from './components/navBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Home/>
     </div>
   );
 }
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div className='myhome'>
+        <div className="home1">
+            <img src="" alt="" />
+            <h1>Gabrielle Alexander</h1>
+            <p>I'm a front-end developer</p>
+            <div className="icons">
+
+            </div>
+            <div className='view-div'>
+                <span className='view'>View my works &darr;</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
