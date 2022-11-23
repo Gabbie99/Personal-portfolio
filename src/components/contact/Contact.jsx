a0084f150cad6e43a61649df7da8c95f7c293146
Gabbie99 <khoinom2018@gmail.com>
2022-11-23 11:44:33 +0100

    chore: on the portfolio

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Contact.css'
-import { MapPin, Envelope, Phone} from 'phosphor-react'
+import { MapPin, Envelope, Phone, WhatsappLogo} from 'phosphor-react'
 
 function Contact() {
   return (
@@ -9,27 +9,55 @@ function Contact() {
           <h1>Contact Me &#128231;</h1>
             <div className="contact2">
                 <div className="contact-text">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse sed voluptates aperiam dolores voluptas velit perspiciatis, quo, quod accusamus neque culpa. Est excepturi officia voluptate molestiae. Sunt, quos quibusdam. Molestias.</p>
-                    <div>
-                    <MapPin size={20} color="#ffffff" weight="fill" />
-                    <span></span>
+                    <p>Let's make something new, different and more meaningful or make something visually pleasing. <span id='sp'>Just Say Hello!</span>.</p>
+                    <div className='con-icon'>
+                    <MapPin size={30} color="#ffffff" weight="light" />
+                    <span>Abuja, Nigeria</span>
                     </div>
-                    <div>
-                    <Envelope size={20} color="#ffffff" weight="fill" />
-                    <span></span>
+                    <div className='con-icon'>
+                    <Envelope size={30} color="#ffffff" weight="light" />
+                    <span><a href='mailto:khoinom2018@gmail.com'>khoinom2018@gmail.com</a></span>
                     </div>
-                    <div>
-                    <Phone size={20} color="#ffffff" weight="fill" />
-                    <span></span>
+                    <div className='con-icon'>
+                    <Phone size={30} color="#ffffff" weight="light" />
+                    <span><a href="tel:+2347062422735">07062422735</a></span>
+                    </div>
+                    <div className='con-icon'>
+                    <WhatsappLogo size={30} color="#ffffff" weight="light" />
+                    <span><a href="https://wa.me/+2348174341196">08174341196</a></span>
                     </div>
                 </div>
                 <div className="contact-inp">
-                  <form action="">
-
+                  <form action="" className='conform'>
+                    <ul>
+                      <div className="contact-list">
+                        <li>
+                          <input type="text" placeholder='Your name*' name='form_name' className='inp' required />
+                        </li>
+                        <li>
+                          <input type="text" placeholder='email*' name='email' className='inp' required />
+                        </li>
+                      </div>
+                      <li>
+                        <input type="text" placeholder='subject' name='text' className='txt' />
+                      </li>
+                      <li>
+                        <textarea name="message" id="context" cols="30" rows="10" placeholder='Message...' />
+                      </li>
+                      <li>
+                        <button type='submit'>Send</button>
+                      </li>
+                    </ul>
                   </form>
                 </div>
             </div>
+            
         </div>
+        <footer>
+          <div className="foot">
+            <p>&copy; 2022 | Made with &#128150; by Gabbie</p>
+          </div>
+        </footer>
     </div>
   )
 }
